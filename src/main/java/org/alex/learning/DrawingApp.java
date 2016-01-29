5f3064f23437df2cf86b4f9772d112e6fa66feaf
alex-gab <dragoi.alex@gmail.com>
2016-01-29 09:19:29 +0200

    spring tutorial 4 to #1

---
+++ src/main/java/org/alex/learning/DrawingApp.java
@@ -0,0 +1,12 @@
+package org.alex.learning;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public final class DrawingApp {
+    public static void main(String[] args) {
+        final BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml");
+        final Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+        triangle.draw();
+    }
+}
---
+++ src/main/java/org/alex/learning/Triangle.java
@@ -0,0 +1,7 @@
+package org.alex.learning;
+
+public final class Triangle {
+    public final void draw() {
+        System.out.println("Triangle drawn!");
+    }
+}
